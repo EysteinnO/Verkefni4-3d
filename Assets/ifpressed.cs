43215d4bf12bf3fb26d5549b0363486326050e5e
EysteinnO <eyssi93@gmail.com>
2018-11-18 20:36:12 +0000

    Verkefni 4 3d

---
+++ Assets/ifpressed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ifpressed : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+    } 
+
+    Animator animate;
+    
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            animate.Play("movepainting");
+            Debug.Log("Entered painting");
+        }
+    }
+    
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
